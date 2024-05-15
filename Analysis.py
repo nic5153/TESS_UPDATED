a397c941f247151ae0d38c44626a3ae912251dfb
Nic McClure <Nic>
2024-05-14 20:27:55 -0500

    adjustments

--- Analysis.py
+++ Analysis.py
@@ -1,9 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from LightCurveObject import LightCurve
+from LCWC import LightCurveWithConstraint
 
-filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector23\Super Outbursts\2020gbb.txt"
+filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector07\Super Outbursts\2019ait.txt"
 
 lc = LightCurve(filename)
+x_min =1514.75
+x_max =1515
+lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max)
 
 lc.plot()
+lc_wc.plot()

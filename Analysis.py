042f01a66c1e9528945dc6e76c5f87174e42654b
Nic McClure <Nic>
2024-05-28 10:44:12 -0500

    updated

--- Analysis.py
+++ Analysis.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 from LightCurveObject import LightCurve
 from LCWC import LightCurveWithConstraint
 
-filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector07\Super Outbursts\2019ait.txt"
+filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector65\2023iwl.txt"
 
 lc = LightCurve(filename)
-x_min =1514.75
-x_max =1515
+x_min =3078
+x_max =3079
 lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max)
 
 lc.plot()

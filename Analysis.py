a2fb9bf24dc981aa7931ec9e4922a0c95372f177
Nic McClure <Nic>
2024-06-01 19:04:26 -0500

    new phase fold arg

--- Analysis.py
+++ Analysis.py
@@ -5,10 +5,15 @@ from LCWC import LightCurveWithConstraint
 
 filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector65\2023iwl.txt"
 
+#3084-3085.5
+
 lc = LightCurve(filename)
-x_min =3078
-x_max =3079
-lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max)
+x_min = 3084
+x_max = 3085.5
+period = 1.81
+lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max, period=period)
 
 lc.plot()
 lc_wc.plot()
+save_dir = r"C:\Users\nic51\OneDrive\Desktop\2023iwl"
+lc_wc.save_plots(save_dir)

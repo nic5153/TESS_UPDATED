1054df641818c861560a810445091773b5949785
Nic McClure <Nic>
2024-06-10 00:07:03 -0500

    added detrend code

--- LCWC.py
+++ LCWC.py
@@ -29,8 +29,10 @@ class LightCurveWithConstraint:
     def lomb_scargle(self):
         max_frequency = 1 / (self.cadence_minutes / 60)
         frequency = np.linspace(0.5, max_frequency, 10000)
+        
+        detrended_brightness = self.detrend()
 
-        ls = LombScargle(self.data[:, 1], self.data[:, 4])
+        ls = LombScargle(self.data[:, 1], detrended_brightness)
         power = ls.power(frequency)
         period_hours = 24 / frequency
         peaks, _ = find_peaks(power, height=0.20)
@@ -42,20 +44,30 @@ class LightCurveWithConstraint:
         return frequency, power, period_hours
 
     def phasefold(self):
+        if self.period is None:
+            raise ValueError("Period must be provided for phase folding.")
         t = self.data[:, 1]
         p = self.period
-        self.phi = (t/p)%1
+        self.phi = (t / p) % 1
         return self.phi
-
+    
+    def detrend(self, degree=2):
+        time = self.data[:, 1]
+        magnitude = self.data[:, 4]
+        coefficients = np.polyfit(time, magnitude, degree)
+        trend = np.polyval(coefficients, time)
+        detrended_magnitude = magnitude - trend + np.mean(trend)
+        return detrended_magnitude
 
     def plot(self, save_dir=None):
         frequency, power, period_hours = self.lomb_scargle()
 
         plt.figure()
-        plt.plot(self.data[:, 1], self.data[:, 4], label="Light Curve W/ Constraint")
+        detrended_magnitude = self.detrend()
+        plt.plot(self.data[:, 1], detrended_magnitude, label="Detrended Light Curve")
         plt.xlabel("Time")
         plt.ylabel("Magnitude")
-        plt.title("Light Curve W/ Constraint")
+        plt.title("Detrended Light Curve with Constraint")
         plt.legend()
         plt.gca().invert_yaxis()
         if save_dir:
@@ -64,31 +76,33 @@ class LightCurveWithConstraint:
             plt.show()
 
         plt.figure()
-        plt.plot(period_hours, power, label="LS Periodogram W/ Constraint")
+        plt.plot(period_hours, power, label="LS Periodogram with Constraint")
         plt.xlabel("Period (hours)")
         plt.ylabel("Power")
-        plt.title("LS Periodogram W/ Constraint")
+        plt.title("LS Periodogram with Constraint")
         plt.legend()
         if save_dir:
             plt.savefig(os.path.join(save_dir, "periodogram.png"))
         else:
             plt.show()
         
-        plt.figure()
-        self.phasefold()
-        sorted_indices = np.argsort(self.phi)
-        plt.plot(self.phi[sorted_indices], self.data[:, 4][sorted_indices], color='r', label="Phase Folded Curve")
-        plt.xlabel("Phase")
-        plt.ylabel("Magnitude")
-        plt.title("Phase Folded Curve")
-        plt.gca().invert_yaxis()
-        plt.legend()
-        if save_dir:
-            plt.savefig(os.path.join(save_dir, "phase_folded_curve.png"))
-        else:
-            plt.show()
+        if self.period is not None:
+            plt.figure()
+            self.phasefold()
+            sorted_indices = np.argsort(self.phi)
+            plt.plot(self.phi[sorted_indices], self.data[:, 4][sorted_indices], color='r', label="Phase Folded Curve")
+            plt.xlabel("Phase")
+            plt.ylabel("Magnitude")
+            plt.title("Phase Folded Curve")
+            plt.gca().invert_yaxis()
+            plt.legend()
+            if save_dir:
+                plt.savefig(os.path.join(save_dir, "phase_folded_curve.png"))
+            else:
+                plt.show()
 
     def save_plots(self, save_dir):
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
         self.plot(save_dir)
+

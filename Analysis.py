b47d02bbd2e38a3b543288b11644a346d5952b62
Nic McClure <Nic>
2024-06-13 19:00:34 -0500

    minor updates

--- Analysis.py
+++ Analysis.py
@@ -1,19 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import os
 from LightCurveObject import LightCurve
 from LCWC import LightCurveWithConstraint
 
 filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Interesting_Curves\sector65\2023iwl.txt"
+save_dir = r"C:\Users\nic51\OneDrive\Desktop\2023iwl"
 
-#3090.5-3095
-
+x_min = 3077
+x_max = 3079
+period = .07867
 lc = LightCurve(filename)
-x_min = 3090.5
-x_max = 3095
-period = .89
+lc.plot()
 lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max, period=period)
 
-lc.plot()
 lc_wc.plot()
-save_dir = r"C:\Users\nic51\OneDrive\Desktop\2023iwl"
-lc_wc.save_plots(save_dir)
+

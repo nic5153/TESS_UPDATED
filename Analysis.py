8890590c2559c610b9abba4d302cd3aedab20c87
Nic McClure <Nic>
2024-07-01 14:41:53 -0500

    added target

--- Analysis.py
+++ Analysis.py
@@ -1,20 +1,22 @@
 from LightCurveObject import LightCurve
 from LCWC import LightCurveWithConstraint
+import numpy as np
 
 
-filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Extracted_Tarballs\TXT_Files\2019pco.txt"
-save_dir = r"C:\Users\nic51\OneDrive\Desktop\Super Outbursts\2019pco"
-x_min = 1725
-x_max = 1731
-period = .068711
+filename = r"C:\Users\nic51\Code_Repos\Tess\Data\Extracted_Tarballs\TXT_Files\2019muu.txt"
+save_dir = r"C:\Users\nic51\OneDrive\Desktop\Super Outbursts\2019muu"
+target = '2019muu'
+x_min = 1689.5
+x_max = 1710
+period = .200988
+power = 0.322009
 
 
-lc = LightCurve(filename)
+lc = LightCurve(filename, target=target)
 lc.plot()
 
-lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max, period=period)
-lc_wc.detrend(max_degree=5)
+lc_wc = LightCurveWithConstraint(filename, x_min=x_min, x_max=x_max, period=period, power=power, target=target)
+lc_wc.detrend(max_degree=4)
 lc_wc.plot()
 lc.save_plots(save_dir)
 lc_wc.save_plots(save_dir)
-

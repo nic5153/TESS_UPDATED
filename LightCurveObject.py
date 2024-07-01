5786584d153b9737888ca28ae81eb9127f7c3173
Nic McClure <Nic>
2024-07-01 14:41:35 -0500

    adjusted plot

--- LightCurveObject.py
+++ LightCurveObject.py
@@ -11,33 +11,26 @@ class LightCurve:
     def _read_data(self):
         return np.loadtxt(self.filename)
 
-    def plot(self):
-        plt.plot(self.data[:, 1], self.data[:, 4], label="Light Curve")
-        plt.xlabel("Time")
-        plt.ylabel("Magnitude")
-        plt.title("Light Curve")
-        plt.legend()
-        plt.gca().invert_yaxis()
-        plt.show()
-
-    def save_plots(self, save_dir=None):
-        if save_dir is None:
-            save_dir = "./"
-
-        if not os.path.exists(save_dir):
-            os.makedirs(save_dir)
-
+    def plot(self, save_dir=None):
         plt.figure()
         plt.plot(self.data[:, 1], self.data[:, 4], label="Light Curve")
-        plt.xlabel("Time")
+        plt.xlabel("Time (BTJD)")
         plt.ylabel("Magnitude")
-        plt.title("Light Curve")
+        plt.title(f"Light Curve of {self.target} ")
         plt.legend()
         plt.gca().invert_yaxis()
-        if self.target is not None:
-            plt.savefig(os.path.join(save_dir, f"light_curve_{self.target}.png"))
+        plt.gcf().set_size_inches(18.5, 10.5)
+
+        if save_dir:
+            if self.target is not None:
+                plt.savefig(os.path.join(save_dir, f"light_curve_{self.target}.png"))
+            else:
+                plt.savefig(os.path.join(save_dir, "light_curve.png"))
         else:
-            plt.savefig(os.path.join(save_dir, "light_curve.png"))
+            plt.show()
         plt.close()
+
+    def save_plots(self, save_dir):
+        self.plot(save_dir)
 
 
